9d6d64ffc0cc681ec47748004aed4d98339068b1
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2018-05-12 20:33:12 +0800

    CreateDatabase

---
+++ app/src/main/java/com/example/android/todolist/database/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.example.android.todolist.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+@Database(entities = { TaskEntry.class }, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "todolist";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getIntance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, AppDatabase.DATABASE_NAME).build();
+            }
+        }
+
+        return sInstance;
+    }
+
+    public abstract TaskDao taskDao();
+}
